d0fd75505f0b9a84ac4e07c71ce5205258d826a0
TheDerickYoung <derickstech@gmail.com>
2023-07-01 19:25:13 -0700

    Changed the home component styling

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,11 +4,11 @@ import Social from "./Social";
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">Welcome 👋</h1>
+      <h1 className="home__title">Hi, I'm Derick 👋</h1>
       {/*<h3 className="home__subtitle">Frontend Developer</h3>*/}
       <p className="home__description">
-        I'm Derick, a passionate and dedicated frontend developer crafting
-        exceptional web experiences. Currently based in Southern-California 📍
+        A passionate and dedicated frontend developer crafting exceptional web
+        experiences. Currently based in Southern-California 📍
       </p>
       <Social />
     </div>
