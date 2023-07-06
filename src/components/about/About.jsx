6f5c3dd3c354a90ce54c854115e37bde07f15f6f
TheDerickYoung <derickstech@gmail.com>
2023-07-06 13:50:20 -0700

    Updated resume .pdf file for download button in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Smith-Cv.pdf";
+import CV from "../../assets/Derick-Young-Jr-Frontend-Engineer-Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
